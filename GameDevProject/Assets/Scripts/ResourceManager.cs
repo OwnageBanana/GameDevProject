84c0c79955e317339fe5f9780a88d9055a19a194
Adam Mills <adammills67@hotmail.com>
2017-11-07 17:03:35 -0400

    basic resource class and manager

---
+++ GameDevProject/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour {
+
+    public static Resources resources;
+
+	// Use this for initialization
+	void Start () {
+
+
+        //if resources isnt initalized create it
+        if (resources == null)
+            resources = new Resources();
+        //other wise inforce singleton and destroy this object
+        else
+            Destroy(gameObject);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GameDevProject/Assets/Scripts/Resources.cs
@@ -0,0 +1,22 @@
+﻿
+public class Resources {
+
+    public Resources()
+    {
+        //baseline food level?
+        Food = 10;
+        Happiness = 100;
+        Energy = 100;
+        ShipIntegrety = 100;
+        Garbage = 0;
+    }
+
+
+    //Each prop can be extended to food being a Food object with expiry, etc
+    public int Food { get; set; } // 0 to cap
+    public int Happiness { get; set; } // 0 to 100
+    public int Energy { get; set; } //0 to cap
+    public int ShipIntegrety { get; set; }// cap to 0
+    public int Garbage { get; set; } // 0 to cap
+
+}
