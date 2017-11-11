4887545ce05cee87b9583b5df7ed3ed865551911
Adam Mills <adammills67@hotmail.com>
2017-11-10 20:16:34 -0400

    Adding the minimap camera and ui, basic following
    To add, selection of area on minimap to move the camera

---
+++ GameDevProject/Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour {
+
+
+    public Transform mainCamera;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+
+
+        //setting the x and z of the minimap camera to of the main camera so it follow
+        Vector3 newPos = mainCamera.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+
+	}
+}
