9a8d440acbee273c4cfaf8835f21f663adbed880
zhangzhidu <zhangzhidu123@gmail.com>
2017-11-25 21:48:05 -0400

    add room select script

---
+++ GameDevProject/Assets/Prefabs/Click.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+    [SerializeField]
+    private LayerMask clickablesLayer;
+	// Use this for initialization
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit rayHit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out rayHit, clickablesLayer))
+            {
+                ClickOn clickOnScript = rayHit.collider.GetComponent<ClickOn>();
+                clickOnScript.currentlySelected = !clickOnScript.currentlySelected;
+                clickOnScript.ClickMe();
+            }
+        }
+	}
+	
+}
